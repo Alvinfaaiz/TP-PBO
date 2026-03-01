f9870f21d41d4ad1bbfaccc455bc60becf5e5bc2
Alvinfaaiz <alvinfaaiz@gmail.com>
2026-03-01 13:28:08 +0700

    upload week 1 dan 2

--- PRAKTIKUM/WEEK 2/TP_MOD2_103012400229_2/src/main/java/com/mycompany/tp_mod2_103012400229_2/TP_MOD2_103012400229_2.java
+++ PRAKTIKUM/WEEK 2/TP_MOD2_103012400229_2/src/main/java/com/mycompany/tp_mod2_103012400229_2/TP_MOD2_103012400229_2.java
@@ -8,9 +8,43 @@ package com.mycompany.tp_mod2_103012400229_2;
  *
  * @author ASUS A15
  */
+
+class Mahasiswa { 
+    
+    private String nama;
+    private String prodi;
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+
+    public void displayMahasiswa() {
+        System.out.println("Nama  : " + nama);
+        System.out.println("Prodi : " + prodi);
+        System.out.println();
+    }
+}
+
 public class TP_MOD2_103012400229_2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Mahasiswa mhs1 = new Mahasiswa();
+        Mahasiswa mhs2 = new Mahasiswa();
+
+        mhs1.setNama("Reza Arap");
+        mhs1.setProdi("Informatika");
+
+        mhs2.setNama("Reza Auditore");
+        mhs2.setProdi("Sistem Informasi");
+
+        mhs1.displayMahasiswa();
+        mhs2.displayMahasiswa();
     }
 }
+
+
