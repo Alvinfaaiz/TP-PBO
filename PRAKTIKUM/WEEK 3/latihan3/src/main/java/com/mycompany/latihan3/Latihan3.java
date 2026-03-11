63f82a5d572a34baea3c0ff2d3918862e6320d27
Alvinfaaiz <alvinfaaiz@gmail.com>
2026-03-11 13:42:26 +0700

    latihan 3

---
+++ PRAKTIKUM/WEEK 3/latihan3/src/main/java/com/mycompany/latihan3/Latihan3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihan3;
+
+/**
+ *
+ * @author ASUS A15
+ */
+public class Latihan3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
